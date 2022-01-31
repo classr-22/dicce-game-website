7ae6642299f22c839549479b667b1fe43e54ca8e
sumit <roundup414@gmail.com>
2022-01-31 14:35:06 +0530

    added three files and images is taken from others repo

---
+++ dom.js
@@ -0,0 +1,25 @@
+var randomno1=Math.floor(Math.random()*6);
+randomno1=randomno1+1;
+if(randomno1==1)
+document.querySelector(".img1").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice1.png";
+else if(randomno1==2)
+document.querySelector(".img1").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice2.png";
+else if(randomno1==3)
+document.querySelector(".img1").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice3.png";
+else if(randomno1==4)
+document.querySelector(".img1").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice4.png";
+else 
+document.querySelector(".img1").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice5.png";
+
+var randomno2=Math.floor(Math.random()*6);
+randomno2=randomno2+1;
+if(randomno2==1)
+document.querySelector(".img2").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice1.png";
+else if(randomno2==2)
+document.querySelector(".img2").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice2.png";
+else if(randomno2==3)
+document.querySelector(".img2").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice3.png";
+else if(randomno2==4)
+document.querySelector(".img2").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice4.png";
+else 
+document.querySelector(".img2").src="https://raw.githubusercontent.com/AhmedAdham2018/Dicee-web-app/e90fa5ddd767655872b4a3d4fde9dfaa5a7ed20b/images/dice5.png";
